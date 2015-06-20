e93f2f6984a9917f03489382a0920a6a3ee9fd41
Kurtis Melby <kurtome@gmail.com>
2015-06-19 21:01:20 -0400

    bumping version and setting zip_safe

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='consul-lock',
-    version='0.1.1',
+    version='0.1.2',
     description='Distributed locking built on top of Consul.',
     url='http://github.com/oysterbooks/python-consul-lock',
     packages=['consul_lock'],
     tests_requires=['mock'],
     install_requires=['python-consul'],
-    zip_safe=False
+    zip_safe=True
 )
