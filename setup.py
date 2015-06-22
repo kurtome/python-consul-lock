176978da15426c767cdde79f5131d62b01186058
Kurtis Melby <kurtome@gmail.com>
2015-06-22 12:50:37 -0400

    bumping version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='consul-lock',
-    version='0.1.4',
+    version='0.1.5',
     description='Distributed locking built on top of Consul.',
     url='http://github.com/oysterbooks/python-consul-lock',
     author='Oyster',
